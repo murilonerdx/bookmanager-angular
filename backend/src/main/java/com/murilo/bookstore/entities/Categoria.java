506d9c411ec541e72bff171f51073fec69926b8a
Murilonerdx <mu-silva@outlook.com>
2021-05-09 15:51:47 -0300

    Criando relacionamentos

---
+++ backend/src/main/java/com/murilo/bookstore/entities/Categoria.java
@@ -0,0 +1,76 @@
+package com.murilo.bookstore.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Categoria implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria(Integer id, String nome, String descricao, List<Livro> livros) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.livros = livros;
+    }
+
+    public Categoria() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) && Objects.equals(nome, categoria.nome) && Objects.equals(descricao, categoria.descricao) && Objects.equals(livros, categoria.livros);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, descricao, livros);
+    }
+}
---
+++ backend/src/main/java/com/murilo/bookstore/entities/Livro.java
@@ -0,0 +1,75 @@
+package com.murilo.bookstore.entities;
+
+import java.util.Objects;
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro(Integer id, String titulo, String nomeAutor, String texto) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+    }
+
+    public Livro() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(id, livro.id) && Objects.equals(titulo, livro.titulo) && Objects.equals(nomeAutor, livro.nomeAutor) && Objects.equals(texto, livro.texto) && Objects.equals(categoria, livro.categoria);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo, nomeAutor, texto, categoria);
+    }
+}
