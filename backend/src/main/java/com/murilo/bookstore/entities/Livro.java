b63e209f442cdbeed9b20df5954ee5367b3fb027
Murilonerdx <mu-silva@outlook.com>
2021-05-09 16:16:01 -0300

    Mentira vou fazer commit sim, pra controlar essa bagaça

--- backend/src/main/java/com/murilo/bookstore/entities/Livro.java
+++ backend/src/main/java/com/murilo/bookstore/entities/Livro.java
@@ -1,5 +1,7 @@
 package com.murilo.bookstore.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Objects;
 
@@ -13,6 +15,7 @@ public class Livro {
     private String texto;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name="categoria_id")
     private Categoria categoria;
 
--- backend/src/main/java/com/murilo/bookstore/resources/CategoriaResource.java
+++ backend/src/main/java/com/murilo/bookstore/resources/CategoriaResource.java
@@ -1,4 +1,23 @@
 package com.murilo.bookstore.resources;
 
+import com.murilo.bookstore.entities.Categoria;
+import com.murilo.bookstore.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
+
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+        Categoria obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
